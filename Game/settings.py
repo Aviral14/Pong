e581c59d583153d84465d90e857c49b1b82662bb
Aviral Agarwal <aviralagarwal25@gmail.com>
2020-01-02 14:13:00 +0530

    Add necessary game settings

---
+++ Game/settings.py
@@ -0,0 +1,14 @@
+# Server Properties
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 8000
+
+# Screen Properties
+SCREEN_HEIGHT = 629
+SCREEN_WIDTH = 1305
+
+# Bar Properties
+BAR_HEIGHT = 150
+BAR_WIDTH = 30
+
+# Score Panel Properties
+PANEL_HEIGHT = 62
