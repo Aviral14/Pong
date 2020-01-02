956fe1c305b8cdde122909a154e8bb5c6301a991
Aviral Agarwal <aviralagarwal25@gmail.com>
2020-01-02 17:54:40 +0530

    Refactor code to load image

--- Game/Pong.py
+++ Game/Pong.py
@@ -5,35 +5,36 @@ from players import *
 from settings import *
 
 
-class Background(pygame.sprite.Sprite):
-    def __init__(self,location):
-        pygame.sprite.Sprite.__init__(self) 
-        self.image = pygame.image.load("bg.png").convert()
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
+def load_image(image, location):
+    image = pygame.image.load(image).convert()
+    rect = image.get_rect()
+    rect.left, rect.top = location
+    return image, rect
 
 
 class Bar(pygame.sprite.Sprite):
-    def __init__(self, no):
-        pygame.sprite.Sprite.__init__(self) 
-        self.image = pygame.image.load("bar.png").convert()
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = (0,0)
-        if no == 1:
-            self.rect.left = 0
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        global player_no
+        if player_no == 1:
+            x = 0
+            player_no=2
         else:
-           self.rect.left = SCREEN_WIDTH-BAR_WIDTH-9
-        self.rect.top = int(SCREEN_HEIGHT / 2 - BAR_HEIGHT)
-        self.up=False
-        self.down=False
+            x = SCREEN_WIDTH - BAR_WIDTH - 9
+            player_no=1
+        y = int(SCREEN_HEIGHT / 2 - BAR_HEIGHT)
+        location = (x, y)
+        self.image, self.rect = load_image("bar.png", location)
+        self.up = False
+        self.down = False
 
     def update(self):
         if self.up:
             self.rect.top -= 13
-            self.up=False
+            self.up = False
         if self.down:
             self.rect.top += 13
-            self.down=False
+            self.down = False
         if self.rect.bottom > SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
         if self.rect.top < PANEL_HEIGHT:
@@ -47,12 +48,12 @@ if __name__ == "__main__":
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Pong")
-    background = Background([0, 0])
+    background_image, background_rect = load_image("bg.png", [0, 0])
 
     clock = pygame.time.Clock()
-    bar = Bar(player_no)
-    allsprites = pygame.sprite.RenderPlain(bar)
-
+    player_bar = Bar()
+    opponent_bar=Bar()
+    allsprites = pygame.sprite.RenderPlain(player_bar,opponent_bar)
 
     while 1:
         clock.tick(60)
@@ -61,13 +62,11 @@ if __name__ == "__main__":
                 sys.exit()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
-            bar.up=True
+            player_bar.up = True
         if keys[pygame.K_DOWN]:
-            bar.down=True
+            player_bar.down = True
 
         allsprites.update()
-        screen.blit(background.image, background.rect)
+        screen.blit(background_image, background_rect)
         allsprites.draw(screen)
         pygame.display.flip()
-                
-
