ad815bdee2ae970761603bf46ab3efd5f9c30681
Aviral Agarwal <aviralagarwal25@gmail.com>
2020-01-08 20:44:14 +0530

    Rename Files

--- Game/pong.py
+++ Game/pong.py
@@ -17,16 +17,16 @@ class Bar(pygame.sprite.Sprite):
             players.player_no = 1
         y = int(SCREEN_HEIGHT / 2 - BAR_HEIGHT)
         location = (x, y)
-        self.image, self.rect = load_image("bar.png", location,transparent=False)
+        self.image, self.rect = load_image("bar.png", location, transparent=False)
         self.up = False
         self.down = False
 
     def update(self):
         if self.up:
-            self.rect.top -= 13
+            self.rect.top -= BAR_SPEED
             self.up = False
         if self.down:
-            self.rect.top += 13
+            self.rect.top += BAR_SPEED
             self.down = False
         if self.rect.bottom > SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
@@ -39,23 +39,32 @@ class Bar(pygame.sprite.Sprite):
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        location = (0,0)
-        self.image, self.rect = load_image("ball.png", location,transparent=True)
-        self.rect.centerx=int(SCREEN_WIDTH/2)
-        self.rect.centery=int(SCREEN_HEIGHT/2)
-        self.speedx=8
-        self.speedy=8
+        self.image, self.rect = load_image("ball.png", transparent=True)
+        self.rect.centerx = int(SCREEN_WIDTH / 2)
+        self.rect.centery = int(SCREEN_HEIGHT / 2)
+        self.speedx = BALL_SPEED
+        self.speedy = BALL_SPEED
 
     def update(self):
-        self.rect.centerx+=self.speedx
-        self.rect.centery+=self.speedy
+        self.rect.centerx += self.speedx
+        self.rect.centery += self.speedy
         if self.rect.bottom > SCREEN_HEIGHT or self.rect.top < PANEL_HEIGHT:
             self.speedy = -self.speedy
         if self.rect.right > SCREEN_WIDTH or self.rect.left < 0:
-            self.rect.centerx =int (SCREEN_WIDTH/2) 
-            self.rect.centery = int(SCREEN_HEIGHT/2)
-    
-def load_image(image, location,transparent=False):
+            self.rect.centerx = int(SCREEN_WIDTH / 2)
+            self.rect.centery = int(SCREEN_HEIGHT / 2)
+
+
+def update_opponent(opponent_bar):
+    if players.opponent_game_state_update:
+        opcode, data = players.opponent_game_state.split(",")
+        data = int(data)
+        if opcode == OP_CODES[0]:
+            opponent_bar.rect.top = data
+        players.opponent_game_state_update = False
+
+
+def load_image(image, location=[0, 0], transparent=False):
     image = pygame.image.load(image).convert()
     image.set_colorkey((255, 255, 255))
     rect = image.get_rect()
@@ -64,11 +73,13 @@ def load_image(image, location,transparent=False):
 
 
 def run_game():
+    # Initializing the game
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Pong")
     background_image, background_rect = load_image("bg.png", [0, 0])
 
+    # Declaring basic entities
     clock = pygame.time.Clock()
     player_bar = Bar()
     opponent_bar = Bar()
@@ -76,8 +87,9 @@ def run_game():
     to_update_sprites = pygame.sprite.RenderPlain(player_bar, ball)
     to_draw_sprites = pygame.sprite.RenderPlain(player_bar, opponent_bar, ball)
 
+    # Game Loop
     while players.going:
-        clock.tick(50)
+        clock.tick(60)
         for event in pygame.event.get():
             if event.type == QUIT:
                 players.going = False
@@ -88,16 +100,11 @@ def run_game():
         if keys[K_DOWN]:
             player_bar.down = True
             players.player_game_state_update = True
-        if players.opponent_game_state_update:
-            opcode, data = players.opponent_game_state.split(",")
-            data = int(data)
-            if opcode == OP_CODES[0]:
-                opponent_bar.rect.top = data
-            players.opponent_game_state_update = False
         if ball.rect.colliderect(player_bar.rect):
-            ball.speedx=-ball.speedx
+            ball.speedx = -ball.speedx
 
         to_update_sprites.update()
+        update_opponent(opponent_bar)
         screen.blit(background_image, background_rect)
         to_draw_sprites.draw(screen)
         pygame.display.flip()
--- Game/start.py
+++ Game/start.py
@@ -1,11 +1,11 @@
 import players
-import Pong
+import pong
 import threading
 
 if __name__ == "__main__":
     player = players.Player()
     player.connect_to_server()
-    game_thread = threading.Thread(target=Pong.run_game)
+    game_thread = threading.Thread(target=pong.run_game)
     peer_thread = threading.Thread(target=player.run)
     game_thread.start()
     peer_thread.start()
